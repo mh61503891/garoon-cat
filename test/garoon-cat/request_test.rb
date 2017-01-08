e14d9ac8bf494b3ca7b18ef11c928ebf3086ca14
Masayuki Higashino <msyk@hgsn.info>
2017-01-08 13:38:11 +0900

    add #test_parse for Request

--- test/garoon-cat/request_test.rb
+++ test/garoon-cat/request_test.rb
@@ -1,10 +1,19 @@
 require 'test_helper'
 
 class GaroonCat::RequestTest < Minitest::Test
+
   def test_to_s
     s = GaroonCat::Request.new.to_s
     puts s
     # require 'pry'
     # binding.pry
   end
+
+  def test_parse
+    require 'rexml/document'
+    assert_no_error {
+      REXML::Document.new(GaroonCat::Request.new.to_s)
+    }
+  end
+
 end
