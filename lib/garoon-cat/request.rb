7f2dc0a1d5a63075d47fe4ec64744243e30abefa
Masayuki Higashino <msyk@hgsn.info>
2017-01-08 14:47:05 +0900

    refactor

--- lib/garoon-cat/request.rb
+++ lib/garoon-cat/request.rb
@@ -1,4 +1,5 @@
 require 'rexml/document'
+require 'time'
 
 module GaroonCat
   class Request
@@ -8,9 +9,17 @@ module GaroonCat
       @username = ENV['USERNAME']
       @password = ENV['PASSWORD']
       @locale = 'jp'
+      @created_at = Time.now # '2010-08-12T14:45:00Z'
+      @expires_at = Time.now # '2037-08-12T14:45:00Z'
     end
 
-    def security
+    def header_action
+      action = REXML::Element.new('Action')
+      action.add_text(@action)
+      action
+    end
+
+    def header_security
       security = REXML::Element.new('Security')
       if @username && @password
         username_token = REXML::Element.new('UsernameToken')
@@ -21,40 +30,51 @@ module GaroonCat
       security
     end
 
-    def locale
+    def header_timestamp
+      timestamp = REXML::Element.new('Timestamp')
+      timestamp.add_element('Created').add_text(@created_at.iso8601)
+      timestamp.add_element('Expires').add_text(@expires_at.iso8601)
+      timestamp
+    end
+
+    def header_locale
       locale = REXML::Element.new('locale')
       locale.add_text(@locale)
       locale
     end
 
-    def to_s
+    def body_action
+      parameters = REXML::Element.new('Parameters')
+      action = REXML::Element.new(@action)
+      action.add_element(parameters)
+      action
+    end
+
+    def doc
+      # header
+      header = REXML::Element.new('soap:Header')
+      header.add_element(header_action)
+      header.add_element(header_security)
+      header.add_element(header_timestamp)
+      header.add_element(header_locale)
+      # body
+      body = REXML::Element.new('soap:Body')
+      body.add_element(body_action)
+      # envelope
+      envelope = REXML::Element.new('soap:Envelope')
+      envelope.add_namespace('soap', 'http://www.w3.org/2003/05/soap-envelope')
+      envelope.add_element(header)
+      envelope.add_element(body)
+      # doc
       doc = REXML::Document.new
       doc << REXML::XMLDecl.new('1.0', 'UTF-8')
-      soap_envelope = REXML::Element.new('soap:Envelope')
-      soap_envelope.add_namespace('soap', 'http://www.w3.org/2003/05/soap-envelope')
-      soap_header = REXML::Element.new('soap:Header')
-      action = REXML::Element.new('Action')
-      action.add_text(@action)
-      timestamp = REXML::Element.new('Timestamp')
-      created = REXML::Element.new('Created')
-      created.add_text('2010-08-12T14:45:00Z')
-      expires = REXML::Element.new('Expires')
-      expires.add_text('2037-08-12T14:45:00Z')
-      timestamp.add_element(created)
-      timestamp.add_element(expires)
-      soap_header.add_element(action)
-      soap_header.add_element(security)
-      soap_header.add_element(timestamp)
-      soap_header.add_element(locale)
-      soap_body = REXML::Element.new('soap:Body')
-      s_method = REXML::Element.new(@action)
-      parameters = REXML::Element.new('Parameters')
-      s_method.add_element(parameters)
-      soap_body.add_element(s_method)
-      soap_envelope.add_element(soap_header)
-      soap_envelope.add_element(soap_body)
-      doc.add_element(soap_envelope)
+      doc.add_element(envelope)
+      doc
+    end
+
+    def to_s
       doc.to_s
     end
+
   end
 end
