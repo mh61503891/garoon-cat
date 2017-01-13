8a49c01f246e59f20585f7f8cc2c3ac004639cff
Masayuki Higashino <msyk@hgsn.info>
2017-01-13 20:21:42 +0900

    bump service

--- lib/garoon-cat/service.rb
+++ lib/garoon-cat/service.rb
@@ -1,58 +1,36 @@
-require 'garoon-cat/request'
-require 'garoon-cat/response'
-require 'active_support/core_ext/hash/conversions'
+require 'garoon-cat/action'
 
-module GaroonCat
-  class Service
+class GaroonCat::Service
 
-    attr_accessor :cookie
+  # @return [GaroonCat::Client]
+  attr_reader :client
 
-    def initialize(params)
-      @params = params
-      @cookie = ''
-    end
+  # @return [String]
+  attr_reader :name
 
-    def method_missing(name, *args)
-      @service = get_service(prefix:@params[:prefix], name:@params[:name], action:name.to_s)
-      @uri = get_uri(endpoint:@params[:endpoint], prefix:@params[:prefix], name:@params[:name])
+  # @return [URI]
+  attr_reader :uri
 
-      # request
-      @service_request = @service.process_request(self, {
-        created_at: @params[:created_at] || Time.now,
-        expires_at: @params[:expires_at] || Time.now,
-        action: @params[:name].camelcase + name.to_s.camelcase,
-        locale: @params[:locale] || 'jp',
-        username: @params[:username],
-        password: @params[:password],
-        parameters: args.empty? ? [] : args[0]
-      })
-      @http_request = Net::HTTP::Post.new(@uri.request_uri, {'Cookie':@cookie})
-      @http_request.body = @service_request.to_s
-      # http
-      @http = Net::HTTP.new(@uri.host, @uri.port)
-      @http.use_ssl = true
-      @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      # @http.set_debug_output $stderr
-
-      # response
-      @http_response = @http.start{ |h| h.request(@http_request) }
-      @service_response = @service.process_response(self, @http_response.body)
-      return @service_response
-    end
-
-    private
-
-    def get_uri(endpoint:, prefix:, name:)
-      URI.parse("#{endpoint}/#{prefix}/#{name}/api?")
-    end
-
-    def get_service(prefix:, name:, action:)
-      require(['garoon-cat', 'services', prefix, name, action.underscore].join('/'))
-      ['GaroonCat', 'Services', prefix.upcase, name.upcase, action.camelcase].join('::').constantize.new
-    rescue LoadError, NameError => e; $stderr.puts e
-      require('garoon-cat/services/default')
-      GaroonCat::Services::Default.new
-    end
-
+  # @param client [GaroonCat::Client]
+  # @param name [String]
+  # @param uri [URI]
+  def initialize(client:, name:, uri:)
+    @client = client
+    @name = name
+    @uri = uri
   end
+
+  private
+
+  # @param key [Symbol] the key of an action.
+  # @return [GaroonCat::Action #execute]
+  def action(key)
+    @actions ||= {}
+    @actions[key] ||= GaroonCat::Action.create(service:self, key:key)
+  end
+
+  def method_missing(key, *args)
+    action(key).execute(args)
+  end
+
 end
--- test/garoon-cat/service_test.rb
+++ test/garoon-cat/service_test.rb
@@ -1,54 +1,19 @@
 require 'test_helper'
-require 'faker'
+require 'uri'
 
 class GaroonCat::ServiceTest < Minitest::Test
 
-  def params
-    {
-      endpoint:ENV['ENDPOINT'],
-      username:ENV['USERNAME'],
-      password:ENV['PASSWORD']
+  def test_service_new_with_error
+    assert_raises(ArgumentError) {
+      GaroonCat::Client.new
     }
   end
 
-  def test_util_login_and_logout
-    service = GaroonCat::Service.new({
-      endpoint:ENV['ENDPOINT'],
-      prefix:'util_api',
-      name:'util'
-    })
-    service.login(login_name:ENV['USERNAME'], password:ENV['PASSWORD'])
-    service.logout()
+  def test_service_new_with_no_error
+    assert_no_error {
+      GaroonCat::Client.new(security:{})
+    }
   end
 
-  def test_base_get_application_status
-    GaroonCat::Service.new(params.merge({
-      prefix:'cbpapi',
-      name:'base',
-    })).get_application_status()
-  end
-
-  def test_base_get_users_by_id
-    GaroonCat::Service.new(params.merge({
-      prefix:'cbpapi',
-      name:'base',
-    })).get_users_by_id(user_id:[1, 2, 3])
-  end
-
-  def test_base_get_user_versions
-    GaroonCat::Service.new(params.merge({
-      prefix:'cbpapi',
-      name:'base',
-    })).get_user_versions()
-  end
-
-  def test_base_get_organization_versions
-    GaroonCat::Service.new(params.merge({
-      prefix:'cbpapi',
-      name:'base',
-    })).get_organization_versions()
-  end
-
-  # .tap{ |e| require 'awesome_print'; ap e }
 
 end
