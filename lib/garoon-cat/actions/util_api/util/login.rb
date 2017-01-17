aa8ec13607f96b7c89599874d444919962431f44
Masayuki Higashino <msyk@hgsn.info>
2017-01-17 16:20:30 +0900

    cleanup

--- lib/garoon-cat/actions/util_api/util/login.rb
+++ lib/garoon-cat/actions/util_api/util/login.rb
@@ -9,45 +9,9 @@ class GaroonCat; class Actions; class UTIL_API; class UTIL
         }
       }))
       request_body = request.to_s
-      # puts request_body
       response_body = @service.client.post(@service.uri, request_body)
       response = GaroonCat::Response.new(response_body)
       return response.to_params
     end
   end
 end; end; end; end
-
-
-
-# def process_response(service, xml)
-#   response = GaroonCat::Response.parse(xml)
-#   service.cookie = response['cookie'][0].strip
-# end
-#
-# end; end; end; end; end
-
-# アクションのキャッシュが必要?
-
-# require 'active_support/core_ext/hash/conversions'
-    # # request
-    # @service_request = @service.process_request(self, {
-    #   parameters: args.empty? ? [] : args[0]
-    # })
-    # define_method('hoge'){}
-
-#   def initialize(uri)
-#     c = GaroonCat::Client.create(uri:uri)
-#     # c.services()
-#     # @uri = uri
-#     # @cookie = nil
-#     # get_hoho()
-
-#     attr_accessor :cookie
-#
-#       @http_request = Net::HTTP::Post.new(@uri.request_uri, {'Cookie':@cookie})
-#       @http_request.body = @service_request.to_s
-#       # http
-#       @http = Net::HTTP.new(@uri.host, @uri.port)
-#       @http.use_ssl = true
-#       @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-#       # @http.set_debug_output $stderr
