2957df994bb998c239ff8423c177594d9ab9ee03
Masayuki Higashino <msyk@hgsn.info>
2017-01-17 16:14:46 +0900

    refactor

---
+++ lib/garoon-cat/actions/cbpapi/base/get_organization_versions.rb
@@ -0,0 +1,9 @@
+require 'garoon-cat/action'
+
+class GaroonCat; class Actions; class CBPAPI; class BASE
+  class GetOrganizationVersions < GaroonCat::Action
+    def execute(*args)
+      super
+    end
+  end
+end; end; end; end
