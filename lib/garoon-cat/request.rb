e7e45a13beeb6e8e90cb951066f1e2f4d2d26e5b
Masayuki Higashino <msyk@hgsn.info>
2017-01-17 15:58:39 +0900

    refactor

--- lib/garoon-cat/request.rb
+++ lib/garoon-cat/request.rb
@@ -1,91 +1,105 @@
 require 'rexml/document'
 require 'time'
 
-module GaroonCat
-  class Request
+class GaroonCat::Request
 
-    def initialize(action:, username:, password:, locale:, created_at:, expires_at:, parameters:)
-      @action = action
-      @username = username
-      @password = password
-      @locale = locale
-      @created_at = created_at
-      @expires_at = expires_at
-      @parameters = parameters
-    end
-
-    def header_action
-      action = REXML::Element.new('Action')
-      action.add_text(@action)
-      action
-    end
-
-    def header_security
-      security = REXML::Element.new('Security')
-      if @username && @password
-        username_token = REXML::Element.new('UsernameToken')
-        username_token.add_element('Username').add_text(@username)
-        username_token.add_element('Password').add_text(@password)
-        security.add_element(username_token)
-      end
-      security
-    end
-
-    def header_timestamp
-      timestamp = REXML::Element.new('Timestamp')
-      timestamp.add_element('Created').add_text(@created_at.iso8601)
-      timestamp.add_element('Expires').add_text(@expires_at.iso8601)
-      timestamp
-    end
-
-    def header_locale
-      locale = REXML::Element.new('locale')
-      locale.add_text(@locale)
-      locale
-    end
-
-    def body_action
-      parameters = REXML::Element.new('parameters')
-      @parameters&.each do |key, value|
-        case value
-        when String
-          parameters.add_element(key.to_s).add_text(value.to_s)
-        when Array
-          value.each do |e|
-            parameters.add_element(key.to_s).add_text(e.to_s)
-          end
-        end
-      end
-      action = REXML::Element.new(@action)
-      action.add_element(parameters)
-      action
-    end
-
-    def doc
-      # header
-      header = REXML::Element.new('soap:Header')
-      header.add_element(header_action)
-      header.add_element(header_security)
-      header.add_element(header_timestamp)
-      header.add_element(header_locale)
-      # body
-      body = REXML::Element.new('soap:Body')
-      body.add_element(body_action)
-      # envelope
-      envelope = REXML::Element.new('soap:Envelope')
-      envelope.add_namespace('soap', 'http://www.w3.org/2003/05/soap-envelope')
-      envelope.add_element(header)
-      envelope.add_element(body)
-      # doc
-      doc = REXML::Document.new
-      doc << REXML::XMLDecl.new('1.0', 'UTF-8')
-      doc.add_element(envelope)
-      doc
-    end
-
-    def to_s
-      doc.to_s
-    end
-
+  def initialize(params)
+    @params = params
   end
+
+  def header_action
+    action = REXML::Element.new('Action')
+    action.add_text(@params.dig(:header, :action).to_s)
+    action
+  end
+
+  def header_security
+    security = REXML::Element.new('Security')
+    username = @params.dig(:header, :security, :username_token, :username)
+    password = @params.dig(:header, :security, :username_token, :password)
+    if username && password
+      username_token = REXML::Element.new('UsernameToken')
+      username_token.add_element('Username').add_text(username)
+      username_token.add_element('Password').add_text(password)
+      security.add_element(username_token)
+    end
+    security
+  end
+
+  def header_timestamp
+    timestamp = REXML::Element.new('Timestamp')
+    timestamp.add_element('Created').add_text(@params.dig(:header, :timestamp, :created).iso8601)
+    timestamp.add_element('Expires').add_text(@params.dig(:header, :timestamp, :expires).iso8601)
+    timestamp
+  end
+
+  def header_locale
+    locale = REXML::Element.new('locale')
+    locale.add_text(@params.dig(:header, :locale))
+    locale
+  end
+
+  def header
+    header = REXML::Element.new('soap:Header')
+    header.add_element(header_action)
+    header.add_element(header_security)
+    header.add_element(header_timestamp)
+    header.add_element(header_locale)
+    header
+  end
+
+  # @todo fix: handling parameters' type
+  def body_action_parameters
+    parameters = REXML::Element.new('parameters')
+    target = @params.dig(:body, :parameters)
+    case target
+    when Hash
+      target.each do |key, value|
+        parameters.add_element(key.to_s).add_text(value.to_s)
+      end
+    end
+    # @params.dig(:body, :parameters)&.each do |key, value|
+    #   case value
+    #   when String
+    #     parameters.add_element(key.to_s).add_text(value.to_s)
+    #   when Array
+    #     value.each do |e|
+    #       parameters.add_element(key.to_s).add_text(e.to_s)
+    #     end
+    #   end
+    # end
+    parameters
+  end
+
+  def body_action
+    action = REXML::Element.new(@params.dig(:header, :action).to_s)
+    action.add_element(body_action_parameters)
+    action
+  end
+
+  def body
+    body = REXML::Element.new('soap:Body')
+    body.add_element(body_action)
+    body
+  end
+
+  def envelope
+    envelope = REXML::Element.new('soap:Envelope')
+    envelope.add_namespace('soap', 'http://www.w3.org/2003/05/soap-envelope')
+    envelope.add_element(header)
+    envelope.add_element(body)
+    envelope
+  end
+
+  def doc
+    doc = REXML::Document.new
+    doc << REXML::XMLDecl.new('1.0', 'UTF-8')
+    doc.add_element(envelope)
+    doc
+  end
+
+  def to_s
+    doc.to_s
+  end
+
 end
--- test/garoon-cat/request_test.rb
+++ test/garoon-cat/request_test.rb
@@ -5,13 +5,23 @@ class GaroonCat::RequestTest < Minitest::Test
 
   def params
     {
-      action:Faker::App.name,
-      username:Faker::Internet.user_name,
-      password:Faker::Internet.password,
-      locale:Faker::Address.country_code.downcase,
-      created_at:Time.now,
-      expires_at:Time.now,
-      parameters:nil
+      header: {
+        action: 'Test',
+        security: {
+          username_token: {
+            username: Faker::Internet.user_name,
+            password: Faker::Internet.password
+          }
+        },
+        timestamp: {
+          created: Time.now,
+          expires: Time.now,
+        },
+        locale: Faker::Address.country_code.downcase
+      },
+      body: {
+        parameters: nil
+      }
     }
   end
 
@@ -19,13 +29,15 @@ class GaroonCat::RequestTest < Minitest::Test
     GaroonCat::Request.new(params)
   end
 
-  def test_to_s
-    $stderr.puts request.to_s
+  def test_new
+    assert_no_error {
+      assert(request)
+    }
   end
 
-  def test_parse
-    require 'rexml/document'
+  def test_to_s
     assert_no_error {
+      require 'rexml/document'
       REXML::Document.new(request.to_s)
     }
   end
